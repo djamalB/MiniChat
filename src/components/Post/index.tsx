863fc26310b7fb3ae0333f0b62ffa320490693a9
djamalB <djamal.balaev@bk.ru>
2024-11-28 15:39:01 +0300

    Изменил дизайн проекта

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,38 +1,35 @@
-
-import { FC } from 'react'
-import classnames from 'classnames'
-import { IPost } from '../Post/IPost';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
-import styles from './Post.module.css';
+import { FC } from "react";
+import classnames from "classnames";
+import { IPost } from "../Post/IPost";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import styles from "./Post.module.css";
 
 interface IPostProps {
-    post: IPost
+  post: IPost;
 }
 
 export const Post: FC<IPostProps> = ({ post }) => {
-    const users = useSelector((state: RootState) => state.users.list)
-    const currentUser = useSelector((state: RootState) => state.users.current)
-    const user = users.find(({id}) => id === post.authorId)
+  const users = useSelector((state: RootState) => state.users.list);
+  const currentUser = useSelector((state: RootState) => state.users.current);
+  const user = users.find(({ id }) => id === post.authorId);
 
-    return (
-        <li
-            className={classnames(styles.post, {
-                [styles.currentUsersPost]: post.authorId === currentUser.id
-            })}
+  return (
+    <li
+      className={classnames(styles.post, {
+        [styles.currentUsersPost]: post.authorId === currentUser.id,
+      })}
+    >
+      <img className={styles.image} src={user?.avatar} alt="author" />
+      <p>
+        <span
+          className={classnames(styles.text, {
+            [styles.currentUsersPostText]: post.authorId === currentUser.id,
+          })}
         >
-            <img
-                className={styles.image}
-                src={user?.avatar}
-                alt="author"
-            />
-            <span
-                className={classnames(styles.text, {
-                    [styles.currentUsersPostText]: post.authorId === currentUser.id
-                })}
-            >
-                {post.text}
-            </span>
-        </li>
-    );
-}
+          {post.text}
+        </span>
+      </p>
+    </li>
+  );
+};
